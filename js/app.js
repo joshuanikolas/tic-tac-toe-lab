d430bec472ff8dfd10cf227423164e1104c45cd0
joshuanikolas <nikolassnchez@icloud.com>
2024-07-31 22:45:43 -0500

    Step 5 complete

--- js/app.js
+++ js/app.js
@@ -6,6 +6,16 @@ const squareEls = document.querySelectorAll('.sqr')
 const messageEl = document.getElementById('message')
         console.log(messageEl)
 
+const winningCombos = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5 ,8],
+[0, 4, 8],
+[2, 4, 6]
+];
 /*---------------------------- Variables (state) ----------------------------*/
 
 let board = ['', '', '', '', '', '', '', '', '']
